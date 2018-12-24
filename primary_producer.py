8738d9cbe5b75c914a601bd4b7176b9f28fceff5
Jorge Aranda <jorge.aranda@cuevano.ca>
2018-12-23 21:34:08 -0800

    Add production cycle to PrimaryProducer

--- primary_producer.py
+++ primary_producer.py
@@ -1,3 +1,4 @@
+from math import ceil, floor
 import secrets
 
 
@@ -5,27 +6,54 @@ class PrimaryProducer():
     def __init__(
         self,
         name=None,
-        labour_per_day=1,
+        units_per_labour_day=1,
+        wages_per_labour_day=1,
         free_storage_units=10,
-        extra_storage_per_unit_day=0.2,
+        storage_cost_per_unit_day=0.2,
         credit=10,
+        max_labourers=1,
         labourers=1,
         stock=0,
-        cash_on_hand=0,
         balance=0,
     ):
         self.name = name if name else secrets.token_hex(8)
-        self.labour_per_day = labour_per_day
+        self.units_per_labour_day = units_per_labour_day
+        self.wages_per_labour_day = wages_per_labour_day
         self.free_storage_units = free_storage_units
-        self.extra_storage_per_unit_day = extra_storage_per_unit_day
+        self.storage_cost_per_unit_day = storage_cost_per_unit_day
         self.credit = credit
+        self.max_labourers = max_labourers
         self.labourers = labourers
         self.stock = stock
-        self.cash_on_hand = cash_on_hand
         self.balance = balance
 
     def __repr__(self):
         return f'<PrimaryProducer {self.name} ' + \
             f'({self.labourers} labourers, ' + \
-            f'{self.stock} stock, {self.cash_on_hand} cash, ' + \
-            f'{self.balance} balance>'
+            f'{self.stock} stock, {self.balance} balance>'
+
+    def tick(self):
+        self.labourers = self._labour_force()
+        self.stock += self._produce()
+        self.stock -= self._dump()
+        self.balance -= self._labour_costs() + self._storage_costs()
+
+    def _labour_force(self):
+        return min(
+            self.max_labourers,
+            floor((self.credit + self.balance) / self.wages_per_labour_day)
+        )
+
+    def _produce(self):
+        return self.labourers * self.units_per_labour_day
+
+    def _dump(self):
+        overdraw = ceil(-self.credit - self.balance + self._storage_costs())
+        return max(0, overdraw)
+
+    def _labour_costs(self):
+        return self.labourers * self.wages_per_labour_day
+
+    def _storage_costs(self):
+        overflow = max(0, self.stock - self.free_storage_units)
+        return overflow * self.storage_cost_per_unit_day
--- test/test_primary_producer.py
+++ test/test_primary_producer.py
@@ -1,9 +1,15 @@
+from pytest import approx
+
 from primary_producer import PrimaryProducer
 
 
 class TestDefaultPrimaryProducer():
     def setup(self):
         self.p = PrimaryProducer()
+
+    def tick_many(self, times):
+        for _ in range(times):
+            self.p.tick()
 
     def test_instantiates(self):
         assert True
@@ -12,8 +18,49 @@ class TestDefaultPrimaryProducer():
         assert self.p.name is not None
         assert self.p.labourers == 1
         assert self.p.stock == 0
-        assert self.p.cash_on_hand == 0
         assert self.p.balance == 0
 
     def test_repr(self):
         assert 'PrimaryProducer' in str(self.p)
+
+    def test_tick(self):
+        self.p.tick()
+        assert self.p.stock == 1
+        assert self.p.balance == -1
+
+    def test_pay_for_storage(self):
+        self.p.balance = 10
+        self.tick_many(11)
+        assert self.p.stock == 11
+        assert self.p.balance == -1.2
+
+    def test_run_out_of_credit(self):
+        self.tick_many(10)
+
+        # At the edge
+        assert self.p.labourers == 1
+        assert self.p.stock == 10
+        assert self.p.balance == -10
+
+        self.p.tick()
+        assert self.p.labourers == 0
+        assert self.p.stock == 10
+        assert self.p.balance == -10
+
+    def test_dump_deadbeat_storage(self):
+        self.p.balance = 2
+        self.tick_many(11)
+
+        # Close to the edge
+        assert self.p.labourers == 1
+        assert self.p.stock == 11
+        assert self.p.balance == -9.2
+
+        self.tick_many(4)
+        assert self.p.labourers == 0
+        assert self.p.stock == 11
+        assert self.p.balance == approx(-10)
+
+        self.p.tick()
+        assert self.p.stock == 10
+        assert self.p.balance == approx(-10)
